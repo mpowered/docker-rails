3dfe8618e084a2601e951bfe41f5ca13f8158bc3
Gary Greyling <greyling.gary@gmail.com>
2017-12-08 11:25:03 +0200

    Update install-wkhtmltopdf.sh
    Fix reference to downloaded package

--- install-wkhtmltopdf.sh
+++ install-wkhtmltopdf.sh
@@ -2,5 +2,5 @@
 
 apt-get install wget
 wget --progress=dot https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb
-dpkg -i wkhtmltox-0.12.2.1_linux-trusty-amd64.deb
+dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
 apt-get install -f -y
