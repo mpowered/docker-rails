ccdc8f778f1a077a09d4117c9ce644f18068ef2f
Gary Greyling <greyling.gary@gmail.com>
2017-12-08 11:29:44 +0200

    Update install-wkhtmltopdf.sh
    Install font-base

--- install-wkhtmltopdf.sh
+++ install-wkhtmltopdf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 apt-get install wget
+apt-get install xfonts-base
+apt-get install xfonts-75dpi
 wget --progress=dot https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.2/wkhtmltox-0.12.2_linux-trusty-amd64.deb
 dpkg -i wkhtmltox-0.12.2_linux-trusty-amd64.deb
 apt-get install -f -y
